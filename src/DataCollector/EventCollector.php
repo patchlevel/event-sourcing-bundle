4acdf3957592043c63d592b67c890598b1803ddb
David Badura <david.badura@patchlevel.de>
2021-12-27 19:33:41 +0100

    fix tests & collect aggregates

--- src/DataCollector/EventCollector.php
+++ src/DataCollector/EventCollector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Patchlevel\EventSourcingBundle\DataCollector;
 
 use Patchlevel\EventSourcing\Aggregate\AggregateChanged;
+use Patchlevel\EventSourcing\Aggregate\AggregateRoot;
 use Symfony\Bundle\FrameworkBundle\DataCollector\AbstractDataCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,24 +13,35 @@ use Throwable;
 
 /**
  * @psalm-type DataType = array{
- *    events: list<AggregateChanged<array<string, mixed>>>
+ *    events: list<AggregateChanged<array<string, mixed>>>,
+ *    aggregates: array<class-string<AggregateRoot>, string>
  * }
  * @psalm-property DataType $data
  */
 class EventCollector extends AbstractDataCollector
 {
     private EventListener $eventListener;
+    /** @var array<class-string<AggregateRoot>, string> */
+    private array $aggregates;
 
-    public function __construct(EventListener $eventListener)
+    /**
+     * @param EventListener $eventListener
+     * @param array<class-string<AggregateRoot>, string> $aggregates
+     */
+    public function __construct(EventListener $eventListener, array $aggregates)
     {
         $this->eventListener = $eventListener;
+        $this->aggregates = $aggregates;
     }
 
     public function collect(Request $request, Response $response, ?Throwable $exception = null): void
     {
         $events = $this->eventListener->get();
 
-        $this->data = ['events' => $events];
+        $this->data = [
+            'events' => $events,
+            'aggregates' => $this->aggregates
+        ];
     }
 
     public static function getTemplate(): string
@@ -43,5 +55,13 @@ class EventCollector extends AbstractDataCollector
     public function getEvents(): array
     {
         return $this->data['events'];
+    }
+
+    /**
+     * @return array<class-string<AggregateRoot>, string> $aggregates
+     */
+    public function getAggregates(): array
+    {
+        return $this->data['aggregates'];
     }
 }
