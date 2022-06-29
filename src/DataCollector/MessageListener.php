b48a81d51a45cbcb6dc52f0f66ca08efda1b1448
David Badura <david.badura@patchlevel.de>
2022-06-29 12:41:30 +0200

    upgrade to 2.0

--- src/DataCollector/MessageListener.php
+++ src/DataCollector/MessageListener.php
@@ -9,16 +9,19 @@ use Patchlevel\EventSourcing\EventBus\Message;
 
 final class MessageListener implements Listener
 {
-    /** @var list<Message> */
+    /** @var list<Message<object>> */
     private array $messages = [];
 
+    /**
+     * @param Message<object> $message
+     */
     public function __invoke(Message $message): void
     {
         $this->messages[] = $message;
     }
 
     /**
-     * @return list<Message>
+     * @return list<Message<object>>
      */
     public function get(): array
     {
