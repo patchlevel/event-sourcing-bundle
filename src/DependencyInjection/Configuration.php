7f0d9c29bb72fa06cd79cff61dec4e77af090eba
Daniel Badura <daniel.badura@patchlevel.de>
2023-06-02 18:13:53 +0200

    Update baseline, add symfony plugin, ignore config errors since they are hard for SA tooling

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Patchlevel\EventSourcingBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
