9a254ce93862fd7eb3374c41c6e86b17fe35901d
David Badura <david.badura@patchlevel.de>
2024-01-03 14:30:25 +0100

    upgrade to beta7

--- tests/Fixtures/CreatedSubscriber.php
+++ tests/Fixtures/CreatedSubscriber.php
@@ -2,13 +2,13 @@
 
 namespace Patchlevel\EventSourcingBundle\Tests\Fixtures;
 
-use Patchlevel\EventSourcing\Attribute\Handle;
+use Patchlevel\EventSourcing\Attribute\Subscribe;
 use Patchlevel\EventSourcing\EventBus\Message;
 use Patchlevel\EventSourcing\EventBus\Subscriber;
 
 class CreatedSubscriber extends Subscriber
 {
-    #[Handle(ProfileCreated::class)]
+    #[Subscribe(ProfileCreated::class)]
     public function onProfileCreated(Message $message): void
     {
         // do nothing
--- tests/Fixtures/Profile.php
+++ tests/Fixtures/Profile.php
@@ -3,16 +3,16 @@
 namespace Patchlevel\EventSourcingBundle\Tests\Fixtures;
 
 use Patchlevel\EventSourcing\Aggregate\BasicAggregateRoot;
+use Patchlevel\EventSourcing\Aggregate\CustomId;
 use Patchlevel\EventSourcing\Attribute\Aggregate;
 use Patchlevel\EventSourcing\Attribute\Apply;
+use Patchlevel\EventSourcing\Attribute\Id;
 
 #[Aggregate('profile')]
 class Profile extends BasicAggregateRoot
 {
-    public function aggregateRootId(): string
-    {
-        return '1';
-    }
+    #[Id]
+    private CustomId $id;
 
     #[Apply]
     protected function applyProfileCreated(ProfileCreated $event): void
