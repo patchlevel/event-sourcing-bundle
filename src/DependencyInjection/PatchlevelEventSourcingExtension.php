9a39dfab9df3c6b53361e15ee30eda9baff22e40
David Badura <david.badura@patchlevel.de>
2024-01-09 13:08:55 +0100

    improve logger

--- src/DependencyInjection/PatchlevelEventSourcingExtension.php
+++ src/DependencyInjection/PatchlevelEventSourcingExtension.php
@@ -43,6 +43,8 @@ use Patchlevel\EventSourcing\EventBus\DefaultEventBus;
 use Patchlevel\EventSourcing\EventBus\EventBus;
 use Patchlevel\EventSourcing\EventBus\Listener;
 use Patchlevel\EventSourcing\EventBus\SymfonyEventBus;
+use Patchlevel\EventSourcing\Metadata\AggregateRoot\AggregateRootMetadataAwareMetadataFactory;
+use Patchlevel\EventSourcing\Metadata\AggregateRoot\AggregateRootMetadataFactory;
 use Patchlevel\EventSourcing\Metadata\AggregateRoot\AggregateRootRegistry;
 use Patchlevel\EventSourcing\Metadata\AggregateRoot\AttributeAggregateRootRegistryFactory;
 use Patchlevel\EventSourcing\Metadata\Event\AttributeEventMetadataFactory;
@@ -244,7 +246,7 @@ final class PatchlevelEventSourcingExtension extends Extension
                 new Reference(ProjectorResolver::class),
                 new Reference(LoggerInterface::class, ContainerInterface::NULL_ON_INVALID_REFERENCE),
             ])
-            ->addTag('monolog.logger', ['channel' => 'projectionist']);
+            ->addTag('monolog.logger', ['channel' => 'event_sourcing']);
 
         $container->setAlias(Projectionist::class, DefaultProjectionist::class);
 
@@ -400,6 +402,9 @@ final class PatchlevelEventSourcingExtension extends Extension
     /** @param Config $config */
     private function configureAggregates(array $config, ContainerBuilder $container): void
     {
+        $container->register(AggregateRootMetadataAwareMetadataFactory::class);
+        $container->setAlias(AggregateRootMetadataFactory::class, AggregateRootMetadataAwareMetadataFactory::class);
+
         $container->register(AttributeAggregateRootRegistryFactory::class);
 
         $container->register(AggregateRootRegistry::class)
@@ -414,7 +419,11 @@ final class PatchlevelEventSourcingExtension extends Extension
                 new Reference(SnapshotStore::class),
                 new Reference(MessageDecorator::class),
                 new Reference('event_sourcing.clock'),
-            ]);
+                new Reference(AggregateRootMetadataFactory::class),
+                new Reference(LoggerInterface::class, ContainerInterface::NULL_ON_INVALID_REFERENCE),
+            ])
+            ->addTag('monolog.logger', ['channel' => 'event_sourcing'])
+        ;
 
         $container->setAlias(RepositoryManager::class, DefaultRepositoryManager::class);
     }
