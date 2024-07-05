38afe0ad978764bddbfa2136be1d0a1ffb85363e
Daniel Badura <daniel.badura@patchlevel.de>
2024-07-05 23:08:25 +0200

    Move into compiler pass, add tests

--- src/PatchlevelEventSourcingBundle.php
+++ src/PatchlevelEventSourcingBundle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Patchlevel\EventSourcingBundle;
 
+use Patchlevel\EventSourcingBundle\DependencyInjection\RepositoryCompilerPass;
 use Patchlevel\EventSourcingBundle\DependencyInjection\TraceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -13,5 +14,6 @@ final class PatchlevelEventSourcingBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new TraceCompilerPass());
+        $container->addCompilerPass(new RepositoryCompilerPass());
     }
 }
